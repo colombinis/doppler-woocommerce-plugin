3e4f95a5467f51ade0d989f5e60f378e69c16d6b
hernangalvan <hernangalvan79@gmail.com>
2019-12-12 11:00:12 -0300

    Feat: on plugin deactivation delete app integration

--- src/includes/class-doppler-for-woocommerce-deactivator.php
+++ src/includes/class-doppler-for-woocommerce-deactivator.php
@@ -32,7 +32,30 @@ class Doppler_For_Woocommerce_Deactivator {
 	 * @since    1.0.0
 	 */
 	public static function deactivate() {
+		
+		$options = get_option('dplr_settings');
+		$has_consumer_secret = get_option('dplrwoo_consumer_secret');
+		
+		if( empty($options['dplr_option_useraccount']) || empty($options['dplr_option_apikey']) ||
+			empty($has_consumer_secret) ) return false;
 
+		//$url = DOPPLER_WOO_API_URL . 'accounts/'.$options['dplr_option_useraccount'].'/integrations/magento';
+		/* DELETE THIS FOR PRODUCTION */
+		$url = 'http://newapiqa.fromdoppler.net/accounts/mariofabianblanc@gmail.com/integrations/magento';
+		$options['dplr_option_apikey'] = '884E71335D719C8F7A37A84F48D7EE6F';
+		/* END DELETE THIS */
+		
+		$response = wp_remote_request($url, array(
+			'method' => 'DELETE',
+			'headers'=> array(
+				"Accept" => "application/json",
+				"Content-Type" => "application/json",
+				"X-Doppler-Subscriber-Origin" => 'WooCommerece',
+				"Authorization" => "token ". $options['dplr_option_apikey']
+			),
+			'timeout' => 12,
+		));
+		var_dump($response); die();
 	}
 
 }
